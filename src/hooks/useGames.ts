45be1b1fe6226ab84307d7b61d55e4834bee0f5c
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-13 20:02:43 -0600

    Finished Building Game Cards

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -2,9 +2,10 @@ import { useState, useEffect } from "react";
 import apiClient from "../services/api-client";
 import { CanceledError } from "axios";
 
-    interface Game {
+    export interface Game {
         id: number;
         name: string;
+        background_image: string;
     }
     
     interface FetchGamesResponse {
@@ -35,3 +36,4 @@ export default function useGames() {
 
   return {games, error}
 }
+
