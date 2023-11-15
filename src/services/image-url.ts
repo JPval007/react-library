6f646dffad9b13cf4d2a3e8f8ee6b8badf0a0a7b
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-14 20:19:49 -0600

    Sort the games

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,6 +1,9 @@
 
 const GetPropImageUrl = (url: string) =>{
+    if (!url) return '';
+    
     const target = 'media/';
+    // Watch out for the error when games have no images
     const index = url.indexOf(target) + target.length;
     return url.slice(0,index) + 'crop/600/400/' + url.slice(index);
 };
