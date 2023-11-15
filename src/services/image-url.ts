245db49c30f3ce437fa35a4e61b59ff0f2dd450b
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-14 20:24:14 -0600

    Handle games with no image

--- src/services/image-url.ts
+++ src/services/image-url.ts
@@ -1,7 +1,9 @@
+import noImage from '../assets/no-image-placeholder-6f3882e0.webp';
+// Import images as a module because in deployment it won't work otherwise, I think mainly beacause of the build folder which has few files
 
 const GetPropImageUrl = (url: string) =>{
-    if (!url) return '';
-    
+    if (!url) return noImage;
+
     const target = 'media/';
     // Watch out for the error when games have no images
     const index = url.indexOf(target) + target.length;
