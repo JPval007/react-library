81b7e9cd0080588058df5d82a3380f9e932e5ca5
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-15 12:24:52 -0600

    Ship genres and platforms with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,5 +1,5 @@
 
-import useData from './useData';
+import genres from "../data/genres";
 
 export interface Genre {
     id: number;
@@ -7,9 +7,11 @@ export interface Genre {
     image_background: string;
 }
 
+// In here we changed the custom hook, we are no longer getting the genres from the server because they don't change
+// and it's also bad UX/UI practice to use too much loading components
+// And it also has minimal impact on server time, less requests, faster performance
 
-
-const useGenres = () => useData<Genre>('/genres');
+const useGenres = () => ({data: genres, isLoading: false, error: null});
 
 
 export default useGenres;
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,5 +1,6 @@
 
 import useData from './useData';
+import platforms from '../data/platforms';
 
 interface Platform {
     id: number;
@@ -7,7 +8,9 @@ interface Platform {
     slug: string;
 }
 
-const usePlatforms = () => useData<Platform>('/platforms/lists/parents');
+// Did the same as genres because the platforms are the same, they won't change for at least 5-10 yrs
+
+const usePlatforms = () => ({data: platforms, isLoading: false, error: null});
     
 
 export default usePlatforms;
