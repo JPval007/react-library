a9a4add868f61acb8881ed2129069babecf177fd
Juan Pablo <juanvalenzuela4business@gmail.com>
2023-11-15 12:50:48 -0600

    Refactored game grid

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -19,7 +19,6 @@ export const GameGrid = ({ gameQuery }: Props) => {
 
   return (
     <>
-      {error && <Text>{error}</Text>}
       <SimpleGrid
         columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
         padding="10px"
